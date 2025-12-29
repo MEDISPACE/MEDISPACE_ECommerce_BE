0db7fb6d95cd89e61227cf8704337821a9c43fd7
nhthongdev <nhthong.dev@gmail.com>
2025-12-29 14:54:00 +0700

    feat: Implement full return request management with dedicated schema, services, and controllers.

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ import chatsRouter from './routes/chats.routes'
 import articlesRouter from './routes/articles.routes'
 import healthCategoriesRouter from './routes/healthCategories.routes'
 import ghnRouter from './routes/ghn.routes'
+import returnRequestsRouter from './routes/returnRequests.routes'
 import { defaultErrorHandler } from '~/middlewares/error.middlewares'
 
 import { initFolder } from './utils/file'
@@ -69,6 +70,7 @@ app.use('/chats', chatsRouter)
 app.use('/articles', articlesRouter)
 app.use('/health-categories', healthCategoriesRouter)
 app.use('/ghn', ghnRouter)
+app.use('/returns', returnRequestsRouter)
 
 
 // Register central error handler so validation and other errors return JSON
